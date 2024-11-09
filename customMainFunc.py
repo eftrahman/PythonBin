3c05e9ebb234dbe7ee1f8ca251ed4e65176033a1
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-11-10 01:06:25 +0600

    Update customMainFunc.py

--- customMainFunc.py
+++ customMainFunc.py
@@ -1,1 +1,13 @@
+global_var = "this is global"
+
+def my_function():
+    global global_var
+    global_var = "globally changed"
+def main():
+    print("before function", global_var)
+    print("after function", global_var)
+    my_function()  # Calling the function inside main
+
+if __name__ == "__main__":
+    main()  # Running main if this script is executed
 
