faeaa8894bb1ed7d4d45fca33a823c0d61a59e3c
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-11-14 23:56:02 +0600

    Update practice01.py

--- practice01.py
+++ practice01.py
@@ -1,1 +1,10 @@
+myInput=None
+myInput=int(input("myInput : "))
+myInput2=int(input("input : "))
+myInput=myInput+myInput2
+print("myInput : ",myInput)
+print("z"*10)
+t1 = 'f'
+t2 = int(input("quantity : "))
+print('f'* t2)
 
