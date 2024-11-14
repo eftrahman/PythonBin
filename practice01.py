d739a5f4e458417f0deaa63b37afc0c9acde70e8
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-11-15 00:00:11 +0600

    Update practice01.py

--- practice01.py
+++ practice01.py
@@ -8,3 +8,11 @@ t1 = 'f'
 t2 = int(input("quantity : "))
 print('f'* t2)
 
+# Python program to demonstrate the application of iskeyword()
+# importing keyword library which has lists
+import keyword
+
+# displaying the complete list using "kwlist()."
+print("The set of keywords in this version is: ")
+print(keyword.kwlist)
+
